e7a0c15b59ada3a07c67de037884551f7363b956
amit96pal <amit96pal.ap@gmail.com>
2022-03-06 22:09:01 +0530

    postman collection

--- src/main/java/canvara/fhir/phr/api/masterdata/controller/MasterDataResource.java
+++ src/main/java/canvara/fhir/phr/api/masterdata/controller/MasterDataResource.java
@@ -14,47 +14,69 @@ public class MasterDataResource {
 
     @GetMapping(value = "/getOrgType")
     public ResponseEntity<List<String>> getOrganizationType() {
-        List<String> orgTypes = Arrays.asList("prov","dept","team","govt","ins","pay","edu","reli","crs","cg","bus","other");
+        List<String> orgTypes = Arrays.asList("prov", "dept", "team", "govt", "ins", "pay", "edu", "reli", "crs", "cg", "bus", "other");
         return ResponseEntity.ok(orgTypes);
     }
+
     @GetMapping(value = "/getContactSystem")
-    public ResponseEntity<List<String>> getContactSystem(){
-        List<String> contactSystems = Arrays.asList("phone","fax","email","paper","url","sms","other");
+    public ResponseEntity<List<String>> getContactSystem() {
+        List<String> contactSystems = Arrays.asList("phone", "fax", "email", "paper", "url", "sms", "other");
         return ResponseEntity.ok(contactSystems);
     }
+
     @GetMapping(value = "/getContactUse")
-    public ResponseEntity<List<String>> getContactUse(){
-        List<String> contactUse = Arrays.asList( "home", "work", "temp", "old", "mobile");
+    public ResponseEntity<List<String>> getContactUse() {
+        List<String> contactUse = Arrays.asList("home", "work", "temp", "old", "mobile");
         return ResponseEntity.ok(contactUse);
     }
+
     @GetMapping(value = "/getAddressUses")
-    public ResponseEntity<List<String>> getAddressUse(){
-        List<String> addressUse = Arrays.asList( "home","work","temp","old","billing");
+    public ResponseEntity<List<String>> getAddressUse() {
+        List<String> addressUse = Arrays.asList("home", "work", "temp", "old", "billing");
         return ResponseEntity.ok(addressUse);
     }
 
     @GetMapping(value = "/getAddressType")
-    public ResponseEntity<List<String>> getAddressType(){
-        List<String> addressType = Arrays.asList( "postal","physical","both");
+    public ResponseEntity<List<String>> getAddressType() {
+        List<String> addressType = Arrays.asList("postal", "physical", "both");
         return ResponseEntity.ok(addressType);
     }
 
     @GetMapping(value = "/getContactPurpose")
-    public ResponseEntity<List<String>> getContactPurpose(){
-        List<String> contactPurpose = Arrays.asList("bill","admin","hr","payor","patinf","press");
+    public ResponseEntity<List<String>> getContactPurpose() {
+        List<String> contactPurpose = Arrays.asList("bill", "admin", "hr", "payor", "patinf", "press");
         return ResponseEntity.ok(contactPurpose);
     }
 
     @GetMapping(value = "/getHumanNameUse")
-    public ResponseEntity<List<String>> getHumanNameUse(){
-        List<String> humanNameUse = Arrays.asList("USUAL","OFFICIAL","TEMP","NICKNAME","ANONYMOUS","OLD","MAIDEN");
+    public ResponseEntity<List<String>> getHumanNameUse() {
+        List<String> humanNameUse = Arrays.asList("usual", "official", "temp", "nickname", "anonymous", "old", "maiden");
         return ResponseEntity.ok(humanNameUse);
     }
 
     @GetMapping(value = "/getEndPointStatus")
-    public ResponseEntity<List<String>> getEndPointStatus(){
-        List<String> endPointStatus = Arrays.asList("active","suspended","error","off","entered_in_error","test");
+    public ResponseEntity<List<String>> getEndPointStatus() {
+        List<String> endPointStatus = Arrays.asList("active", "suspended", "error", "off", "entered_in_error", "test");
         return ResponseEntity.ok(endPointStatus);
+    }
+
+    // location api specific
+    @GetMapping(value = "/getLocStatus")
+    public ResponseEntity<List<String>> getLocationStatus() {
+        List<String> locStatus = Arrays.asList("active", "suspended", "inactive");
+        return ResponseEntity.ok(locStatus);
+    }
+
+    @GetMapping(value = "/getLocMode")
+    public ResponseEntity<List<String>> getLocationModes() {
+        List<String> locModes = Arrays.asList("instance", "kind");
+        return ResponseEntity.ok(locModes);
+    }
+
+    @GetMapping(value = "/getDays")
+    public ResponseEntity<List<String>> getDays() {
+        List<String> days = Arrays.asList("mon", "tue", "wed", "thu", "fri", "sat", "sun");
+        return ResponseEntity.ok(days);
     }
 
 }
